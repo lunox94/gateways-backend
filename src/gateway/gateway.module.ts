3d8f03998d38591db5fb37eb74efaa98271ca7ac
lunox94 <gmrlunox94@gmail.com>
2022-03-28 00:54:32 -0400

    fix: fix import :)

--- src/gateway/gateway.module.ts
+++ src/gateway/gateway.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { GatewayController } from './gateway.controller';
-import { GatewayService } from './gateway/gateway.service';
 import { GatewayService } from './gateway.service';
 
 @Module({
   controllers: [GatewayController],
-  providers: [GatewayService]
+  providers: [GatewayService],
 })
 export class GatewayModule {}
