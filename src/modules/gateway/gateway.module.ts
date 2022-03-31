c02e99d9dd4d12695238fdd961ebbeeaf3e12cc4
lunox94 <gmrlunox94@gmail.com>
2022-03-31 19:58:20 -0400

    feat: add repository layer

--- src/modules/gateway/gateway.module.ts
+++ src/modules/gateway/gateway.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { gateways } from 'src/mock/gateways.data';
 import { GatewayController } from './gateway.controller';
 import { GatewayService } from './gateway.service';
+import { Repository } from 'src/infrastructure/repository';
+import { InMemoryRepository } from 'src/infrastructure/in-memory-repository';
+import { InMemoryDb } from 'src/common/constants/constants';
 
 @Module({
-  controllers: [GatewayController],
-  providers: [GatewayService],
+    controllers: [GatewayController],
+    providers: [
+        GatewayService,
+        {
+            provide: Repository,
+            useClass: InMemoryRepository,
+        },
+        {
+            provide: InMemoryDb,
+            useValue: gateways,
+        },
+    ],
 })
 export class GatewayModule {}
